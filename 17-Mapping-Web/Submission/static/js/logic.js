a54a706aa8bfa37c3c7bfb2c0ead489432ee099f
Scott Frazier <68704290+scottfrazier97@users.noreply.github.com>
2021-02-13 14:15:03 -0600

    Update logic.js

--- 17-Mapping-Web/Submission/static/js/logic.js
+++ 17-Mapping-Web/Submission/static/js/logic.js
@@ -8,26 +8,20 @@ $(document).ready(function() {
 });
 
 function makeMap() {
-    // data
-    // set title
     var timeframe_text = $("#timeframe option:selected").text();
     $("#maptitle").text(`This represents all earthquakes recorded by the USGS for the ${timeframe_text}`);
 
-    // Store our API endpoint as queryUrl
     var timeframe = $("#timeframe").val();
     var queryUrl = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${timeframe}.geojson`
 
-    // Perform a GET request to the query URL
     $.ajax({
         type: "GET",
         url: queryUrl,
         success: function(data) {
-            // make second call
             $.ajax({
                 type: "GET",
                 url: "/static/data/PB2002_boundaries.json",
                 success: function(tectonic) {
-                    //BUILD WITH BOTH DATASETS
                     buildMap(data, tectonic);
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -46,9 +40,7 @@ function makeMap() {
 function buildMap(data, tectonic) {
     $("#mapcontainer").empty();
     $("#mapcontainer").append(`<div id="mapid"></div>`);
-
-    // Step 0: Create the Tile Layers
-    // Add a tile layer
+    
     var dark_mode = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -67,14 +59,12 @@ function buildMap(data, tectonic) {
         accessToken: API_KEY
     });
 
-    // STEP 1: INIT MAP
     var myMap = L.map("mapid", {
         center: [33.0, -96.0],
         zoom: 4,
         layers: [light_mode, dark_mode]
     });
 
-    //Step 2: Create Markers
     var earthquakes = [];
     var circle_list = [];
     data.features.forEach(function(earthquake) {
@@ -93,7 +83,6 @@ function buildMap(data, tectonic) {
         circle_list.push(circle);
     });
 
-    // create tectonic plates
     var tectonic_plates = L.geoJSON(tectonic, {
         color: "orange",
         weight: 3
@@ -104,8 +93,7 @@ function buildMap(data, tectonic) {
     var marker_group2 = L.layerGroup(circle_list);
     var tectonic_layer = L.layerGroup([tectonic_plates]); // note this has to be a list
 
-    //STEP 3: Create Layers
-    // Create Layer Legend
+ 
     var baseMaps = {
         "Light Mode": light_mode,
         "Dark Mode": dark_mode
@@ -117,21 +105,15 @@ function buildMap(data, tectonic) {
         "Tectonic Plates": tectonic_layer
     };
 
-    // Slap Layer Legend onto the map
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
-    // add layers pre-clicked to map
     tectonic_plates.addTo(myMap);
     marker_group2.addTo(myMap);
 
-    // Step 4: CREATE THE LEGEND (of Zelda)
-
-    // Set up the legend
     var legend = L.control({ position: "bottomright" });
     legend.onAdd = function() {
         var div = L.DomUtil.create("div", "info legend");
 
-        // create legend as raw html
         var legendInfo = `<h4 style = "margin-bottom:10px"> Earthquake Depth </h4>
         <div>
         <div style = "background:#98ee00;height:10px;width:10px;display:inline-block"> </div> 
@@ -162,7 +144,6 @@ function buildMap(data, tectonic) {
         return (div)
     }
 
-    // Adding legend to the map
     legend.addTo(myMap);
 
 }
@@ -197,9 +178,7 @@ function createMarkerOptions(feature) {
     return (geojsonMarkerOptions)
 }
 
-// called in the create circles
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
     if (feature.properties && feature.properties.place) {
         layer.bindPopup(feature.properties.title);
     }
